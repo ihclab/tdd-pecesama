be0831589e7e2e3805cc78b16b0f9e5e60220d29
Pedro Santana <pecesama@gmail.com>
2019-03-20 14:07:50 -0600

    Estructura de la clase ordenamiento Mac

---
+++ Ordenamiento/Burbuja.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordenamiento {
+    public class Burbuja {
+        throw new NotImplementedException("Crear los casos de prueba");
+    }
+}
