5bf38647cecf2f4e6e602bd1667c5a35c6a7be83
Pedro <pecesama@gmail.com>
2019-03-20 15:07:55 -0600

    Agregado el proyecto de pruebas

---
+++ Ordenamiento.Pruebas/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ordenamiento.Pruebas
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
