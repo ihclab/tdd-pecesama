db5fc5b8e938f608a3b30698bef0dce2c44541db
Pedro <pecesama@gmail.com>
2019-03-20 15:57:33 -0600

    Con el método de ordenamiento

--- Ordenamiento/Burbuja.cs
+++ Ordenamiento/Burbuja.cs
@@ -5,7 +5,14 @@ using System.Text;
 namespace Ordenamiento {
     public class Burbuja {
         public void Ordenar(int[] arr) {
-            throw new NotImplementedException("Crear los casos de prueba");
+            //throw new NotImplementedException("Crear los casos de prueba");
+            for (int i = 0; i < arr.Length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int aux = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = aux;
+                }
+            }
         }
     }
 }
